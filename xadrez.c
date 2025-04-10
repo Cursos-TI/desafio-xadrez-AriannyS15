ce6703a6044b15df9785f4a5b70c6a7af36ba38e
AriannyS15 <ariannythalita285@gmail.com>
2025-04-10 23:55:52 +0000

    nivelnovato

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,30 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+ int main() {
+    int i = 1;
+  
+   //movendo a torre 5 casas para a direita
+    for (int i = 0; i < 5; i++)
+    {
+        printf ("Direita\n"); //imprimindo a direção do movimento
+    }
+    
+   //movendo a bispo 4 casas para a cima, direita
+ do
+ {
+    printf("Cima, Direita\n");//imprimindo a direção do movimento
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    ++i;
+ } while (i <= 4);
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+  //movendo a rainha 8 casas para a esquerda
+ while (i <= 8)
+ {
+   printf("Esquerda\n");//imprimindo a direção do movimento
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+   i++;
+ }
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
