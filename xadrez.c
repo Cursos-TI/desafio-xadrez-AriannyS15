5098bfe25f7cab128c3f976ce0677a7bb281428d
AriannyS15 <ariannythalita285@gmail.com>
2025-04-14 21:40:02 +0000

    nivelmestre

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,56 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+void movertorre(int casas){
+  if (casas > 0){
+    printf("Direita\n");
+    movertorre(casas - 1);
+  }
+}
+
+void moverbispo(int casas){
+  if (casas > 0){
+    printf("Cima, Direita\n");
+    moverbispo(casas - 1);
+  }
+}
+
+void moverrainha(int casas){
+  if (casas > 0){
+    printf("Esquerda\n");
+    moverrainha(casas - 1);
+  }
+}
+
+void movercavalo(int casas){
+ for (int i = 1; i <= 3; i++) {
+     if (i < 3) {
+         printf("Cima\n");
+         continue;  
+     }
+     if (i == 3) {
+         printf("Direita\n");
+         break; 
+     }
+     printf("%d\n", i);
+ }
+
+}
 
  int main() {
-    int i = 1;
-    int movimentocompleto = 1; //controlar o movimento em 'L'
+  printf("***Torre***\n");
+  movertorre(5);
+ 
+  printf("***Bispo***\n");
+  moverbispo(4);
 
-  //Nivel Novato - Movimentação de peças
-   //movendo a torre 5 casas para a direita
-    for (int i = 0; i < 5; i++)
-    {
-        printf ("Direita\n"); //imprimindo a direção do movimento
-    }
-    
-   //movendo a bispo 4 casas para a cima, direita
- do
- {
-    printf("Cima, Direita\n");//imprimindo a direção do movimento
+  printf("***Rainha***\n");
+  moverrainha(8);
 
-    ++i;
- } while (i <= 4);
+  printf("***Cavalo***\n");
+  movercavalo(3);
 
-  //movendo a rainha 8 casas para a esquerda
- while (i <= 8)
- {
-   printf("Esquerda\n");//imprimindo a direção do movimento
-
-   i++;
- }
-    // Nível Aventureiro - Movimentação do Cavalo
-    //Criando loops aninhados
-   do
-   {
-    for ( int i = 0; i < 2; i++)
-    {
-        printf("Cima\n"); //imprimindo 'cima' duas vezes
-    }
-
-    printf("Direita\n"); //imprimindo direita
-
-   } while (movimentocompleto--);
+  return 0;
+}
 
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
@@ -51,5 +60,5 @@
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
-}
+  
+
